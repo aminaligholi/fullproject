654216fd98336547bf674e6df3c801fdb0d8a66a
aminaligholi <amin.aligholi@gmail.com>
2020-11-08 15:50:47 +0330

    add fun page

---
+++ pages/pagetest/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+      <section >
+          <h1>random</h1>
+      </section>
+  </div>
+</template>
+
+<script>
+
+export default{
+
+
+
+}
+
+</script>
+
+<style scoped>
+ section{
+     width: 100%;
+     height: 100vh;
+     /* background-image: url("../../assets/4.jpg"); */
+     display: flex;
+     justify-content: center;
+     align-items: center; 
+     
+ }
+ section h1 {
+     padding: 10px 15px;
+     color: rgb(0, 0, 0);
+     background: rgb(255, 255, 255);
+     font-size: 15vh;
+     mix-blend-mode:screen;
+     font-weight: 900;
+
+
+ }
+</style>
